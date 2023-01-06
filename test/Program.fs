87cb17dd4d3ea020a56e59fc2428b6fa573e43b3
Humza Shahid <humzasaur@gmail.com>
2023-01-06 04:15:34 +0000

    refactor tests to use public interface type

--- test/Program.fs
+++ test/Program.fs
@@ -1,15 +1,5 @@
 open FsRope
 open System.Collections
-open FsRope.Rope
-
-[<Literal>]
-let text =
-    "During the development of the .NET Framework, the class libraries were originally written using a managed code compiler system called \"Simple Managed C\" (SMC)."
-
-[<Literal>]
-let insText = "TEST!"
-
-let initialTable = Rope.create text
 
 module Program =
     [<EntryPoint>]
