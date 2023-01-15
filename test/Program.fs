02e658f927024cdd2b5662c2c15e91e5f6187422
Humza Shahid <74657165+hummy123@users.noreply.github.com>
2023-01-15 10:00:30 +0000

    clean

--- test/Program.fs
+++ test/Program.fs
@@ -1,17 +1,4 @@
-open FsRope
-open FsRope.Rope
-open System.Text
-
 module Program =
     [<EntryPoint>]
     let main _ =  
-        let b = StringBuilder()
-        let mutable rope = Rope.empty
-        for i in [0..10] do
-            b.Append(i.ToString()) |> ignore
-            rope <- rope.Apeend(i.ToString())
-            b.Append(" ") |> ignore
-            rope <- rope.Apeend(" ")
-            printfn "%A" <| b.ToString()
-            printfn "%A" <| rope.Text()
         0
